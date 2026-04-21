993bf397f3bbb90f3e18aa64c750de1e66f0ddb0
devika-builds <vikkir29@gmail.com>
2026-04-21 14:22:02 -0400

    Delete zapier-visualizer directory

--- zapier-visualizer/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ZapierVisualizer from "./ZapierVisualizer.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ZapierVisualizer />
-  </React.StrictMode>
-);
